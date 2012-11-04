43fb79b61bc712add8b52c38b554c8bfbe8a6115
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-04 16:50:30 +0000

    form update

--- src/templates/createtest.php
+++ src/templates/createtest.php
@@ -9,7 +9,7 @@
                     <ul>
                         <li><a href="#tab1" data-toggle="tab">Test Manager</a></li>
                         <li><a href="#tab2" data-toggle="tab">Questions</a></li>
-                        
+
                     </ul>
                 </div>
             </div>
@@ -36,7 +36,13 @@
                             <div class="control-group">
                                 <label class="control-label" for="subject">Test Subject</label>
                                 <div class="controls">
-                                    <input name="subject" type="text" id="subject" />
+                                    <select name="subject" id="subject">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
                                 </div>
                             </div>
 
@@ -55,7 +61,6 @@
                         </td>
                     </tr>
                     <tr>
-
                         <td>
                             <div class="control-group">
                                 <label class="control-label" for="instructions">Instructions</label>
@@ -98,36 +103,89 @@
                                         <input name="testtime" type="text" id="testtime" class="span1"  />
                                         <span class="add-on">Mins</span>
                                     </div>
-
                                 </div>
                             </div>
 
                         </td>
 
 
-
-                    </tr>
-                    <tr>
-                        <td>
-
-
-                            <div class="control-group">
-                                <div class="controls">
-                                    <button type="submit" class="span2 btn">Create</button>
-                                </div>
-                            </div>
-                        </td>
 
                     </tr>
                 </table>	      
-
-
-
             </div>
             <div class="tab-pane" id="tab2">
-                2
+                <div class="control-group">
+                    <label class="control-label" for="question_type">Question Type</label>
+                    <div class="controls">
+                        <select name="question_type" id="question_type">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="question">Question</label>
+                    <div class="controls">
+                        <textarea cols="45" rows="5" name="question"></textarea>
+                    </div>
+                </div>
+                <div>
+                    <table class="table table-striped table-bordered table-hover .table-condensed">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Answer</th>
+                                <th>Match Answer</th>
+                                <th>Is Correct</th>
+                                <th>Correctness Percentage</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <label>1</label>
+                                </td>
+                                <td>
+                                    <textarea rows="1" name="answer"></textarea>
+                                </td>
+                                <td>
+                                    <textarea rows="1" name="match_answer"></textarea>
+                                </td>
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="">
+                                        Multiple Option
+                                    </label>
+                                    <label class="radio">
+                                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                                        Correct
+                                    </label>
+                                </td>
+                                <td>
+                                    <div class="controls" style="margin-left: 0px">
+                                        <div class="input-append" >
+                                            <input name="correctness_percentage" type="text" id="correctness_percentage" class="span1"/>
+                                            <span class="add-on">%</span>
+                                        </div>
+                                    </div>
+
+                                </td>
+                                <td>
+                                    <i class="icon-edit"></i>
+                                    <i class="icon-trash"></i>
+                                    <i class="icon-ok"></i>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            
+
+
             <ul class="pager wizard">
                 <li class="previous first" style="display:none;"><a href="#">First</a></li>
                 <li class="previous"><a href="#">Previous</a></li>
