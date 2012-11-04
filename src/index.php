310ee11884b657fa51d0defa4984f97a79075583
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-04 13:10:02 +0000

    create test form

--- src/index.php
+++ src/index.php
@@ -1,7 +1,88 @@
 <?php
+define('AppRoot', dirname(dirname(__FILE__)));
+include_once AppRoot . '/inc/config/properties.php';
+include_once AppRoot . '/inc/cUtil.php';
+$utilObj = new cUtil();
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$_GET = $utilObj->urlDecode($_GET);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>GExperting</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Le styles -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <style>
+            body {
+                padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+            }
+        </style>
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+        <script src="js/jquery.js"  ></script>
+        <script src="js/jquery-ui.js"  ></script>
+        <script src="js/bootstrap.min.js"  ></script>
+
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Fav and touch icons -->
+        <link rel="shortcut icon" href="img/favicon.ico">
+
+    </head>
+
+    <body>
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Project name</a>
+                    <a class="brand" href="<?php echo $utilObj->createLinkUrl(array('f' => 'createtest')); ?>">Create Test</a>
+
+                    <div class="btn-group pull-right">
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            Action
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>Settings</li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav ">
+
+                            <li><a href="#contact">Login</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <?php
+            $page = $_GET['f'] ? $_GET['f'] : 'home';
+            include 'templates/' . $page . '.php';
+            ?>
+
+        </div> <!-- /container -->
+
+
+    </body>
+</html>
+
