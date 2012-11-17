a9648949ef0968296fd99ab4665646f37c89e1bb
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-17 09:15:33 +0000

    ui changes and code cleanup

--- src/templates/take_a_test.php
+++ src/templates/take_a_test.php
@@ -1,47 +1,25 @@
 <form method="POST" class="form-horizontal">
 
-    <div id="rootwizard">
-        <div class="navbar">
-            <div class="navbar-inner">
-                <div class="container">
-                    <ul>
-                        <li><a href="#tab1" data-toggle="tab">Take a Test</a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="tab-content">
-            <div class="tab-pane" id="tab1">
-
-                <legend>Tests Available</legend>  
-                <table class="table table-striped" id="datatable">  
+    <legend>Tests Available</legend>  
+    <table class="table table-striped" id="datatable">  
         <thead>  
-          <tr>  
-            <th>Test Name</th>  
-            <th>Status</th>  
-            <th>Take Test</th>            
-          </tr>  
+            <tr>  
+                <th>Test Name</th>  
+                <th>Status</th>  
+                <th>Take Test</th>            
+            </tr>  
         </thead>  
         <tbody>  
-          <tr>  
-            <td>Excel Experting for Entry level test</td>  
-            <td>Available </td>  
-            <td><a href="test1.html">Take Test</a></td>  
-          </tr>  
-          <tr>  
-            <td>MS Powerpoint Experting for Entry level test</td>  
-            <td>Available </td>  
-            <td><a href="test1.html">Take Test</a></td>  
-          </tr>         </tbody>  
-      </table>  
+            <tr>  
+                <td>Excel Experting for Entry level test</td>  
+                <td>Available </td>  
+                <td><a href="test1.html">Take Test</a></td>  
+            </tr>  
+            <tr>  
+                <td>MS Powerpoint Experting for Entry level test</td>  
+                <td>Available </td>  
+                <td><a href="test1.html">Take Test</a></td>  
+            </tr>         </tbody>  
+    </table>  
 
-        </div>	
-    </div>
-    </div>
 </form>
-<script>
-    $(document).ready(function() {
-        $('#rootwizard').bootstrapWizard();
-    });
-</script>
--- src/templates/tests.php
+++ src/templates/tests.php
@@ -1,73 +1,16 @@
 <form method="POST" class="form-horizontal">
+    <a  href="<?php echo $cFormObj->createLinkUrl(array('f' => 'createtest')); ?>">New Test</a>
+    <legend>Tests Available</legend>  
+    <?php
+    $cTestControllerObj->table = "test_details";
+    $cTestControllerObj->curd();
+    $cFormObj->options['actioncolumn'] = true;
+    $cFormObj->options['header'] = array("Id", "Name", "Code", "Logo", "Created", "Status");
+    $cFormObj->createHTable();
+    echo $cFormObj->html();
+    ?>
 
-    <div id="rootwizard">
-        <div class="navbar">
-            <div class="navbar-inner">
-                <div class="container">
-                    <ul>
-                        <li><a href="#tab1" data-toggle="tab">Test Manager</a></li>
-                        <li><a href="#tab2" data-toggle="tab">Questions</a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="tab-content">
-            <div class="tab-pane" id="tab1">
-
-                <legend>Tests Available</legend>  
-                <table class="table table-striped" id="datatable">  
-        <thead>  
-          <tr>  
-            <th>Test Name</th>  
-            <th>Category</th>  
-            <th>Active Dates</th>  
-            <th>Active</th>  
-            <th>Actions</th>  
-
-          </tr>  
-        </thead>  
-        <tbody>  
-          <tr>  
-            <td>001</td>  
-            <td>Rammohan </td>  
-            <td>Reddy</td>  
-            <td>A+</td>  
-                                <td>
-                                    <i class="icon-edit"></i>
-                                    <i class="icon-trash"></i>
-                                    <i class="icon-ok"></i>
-                                </td>          
-
-          </tr>  
-
-          <tr>  
-            <td>003</td>  
-            <td>Rabindranath</td>  
-            <td>Sen</td>  
-            <td>A+</td>  
-                                <td>
-                                    <i class="icon-edit"></i>
-                                    <i class="icon-trash"></i>
-                                    <i class="icon-ok"></i>
-                                </td>          </tr>  
-        </tbody>  
-      </table>  
-
-            <ul class="pager wizard">
-                <li class="previous first" style="display:none;"><a href="#">First</a></li>
-                <li class="previous"><a href="#">Previous</a></li>
-                <li class="next last" style="display:none;"><a href="#">Last</a></li>
-                <li class="next"><a href="#">Next</a></li>
-            </ul>
-        </div>	
-    </div>
 </form>
-<script>
-    $(document).ready(function() {
-        $('#rootwizard').bootstrapWizard();
-    });
-</script>
 
 
 
