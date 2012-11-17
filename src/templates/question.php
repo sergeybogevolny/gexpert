ebebffe5d740686a63ac39232fc7530b650bf64a
smartguy07 <smartguy07@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-17 08:43:44 +0000

    Question types

--- src/templates/question.php
+++ src/templates/question.php
@@ -114,104 +114,33 @@ numbers is to click in the cell below the numbers and then:</b></label></br>
 
                 <legend>Question (4 of 10)</legend>  
                 <div class="control-group">
-                    <label class="control-label" for="question1"><b> Match the following file extensions</b></label></br>
+                    <label class="control-label" for="question1"><b>Arrange in Right Sequence</b></label></br>
                 <table class="table table-striped table-bordered table-hover .table-condensed">
                         <thead>
                             <tr>
-                                <th>Question</th>                              
-                                <th>Match Answer</th>
-                            </tr>
+                                <th>Arrange in Right Sequence</th>                              
+                                                          </tr>
                         </thead>
                         <tbody>
-                            <tr>
+               <tr>
                                 
                                 <td>
-                                    
-                                    <label class="control-label inline">MS Word</label>
-                                    <select class="span1">
-                                           <option>Select</option>                                           
-                                           <option>A</option>   
-                                           <option>B</option>                                          
-                                           <option>C</option>                                          
-                                           <option>D</option>                                          
-                                        </select>
-                                </td>
-                                <td>
-                                    <label class="control-label inline">.mdb</label>
-                                    <div class="input-append">
-                                        <span class="add-on"><strong>A</strong></span>
-                                   </div>
-                                </td>
-                                                               
-                            </tr>
-                            <tr>
-                                
-                                <td>
-                                    
-                                    <label class="control-label">MS Excel</label>
-                                    <select class="span1">
-                                           <option>Select</option>                                           
-                                           <option>A</option>   
-                                           <option>B</option>                                          
-                                           <option>C</option>                                          
-                                           <option>D</option>                                          
-                                        </select>
-                                </td>
-                                <td>
-                                    <label class="control-label">.ppt</label>
-                                    <div class="input-append">
-                                        <span class="add-on"><strong>B</strong></span>
-                                   </div>
-                                </td>
+                                    <ul id="sortable">
+                                       <li class="ui-state-default">mdb</li>
+                                       <li class="ui-state-default">doc</li>
+                                       <li class="ui-state-default">xls</li>
+                                       <li class="ui-state-default">ppt</li>                                       
+                                   </ul>
                                
-                            </tr>
-                            <tr>
-                                
-                                <td>
-                                    
-                                    <label class="control-label inline">MS Accss</label>
-                                    <select class="span1">
-                                           <option>Select</option>                                                                                   
-                                           <option>A</option>   
-                                           <option>B</option>                                          
-                                           <option>C</option>                                          
-                                           <option>D</option>                                          
-                                        </select>
-                                </td>
-                                <td>
-                                    <label class="control-label inline">.xls</label>
-                                    <div class="input-append">
-                                        <span class="add-on"><strong>C</strong></span>
-                                   </div>
-                                </td>
-                              
-                            </tr>
-                            <tr>
-                                
-                                <td>
-                                    
-                                    <label class="control-label inline">MS Powerpoint</label>
-                                    <select class="span1">
-                                           <option>Select</option>                                           
-                                           <option>A</option>   
-                                           <option>B</option>                                          
-                                           <option>C</option>                                          
-                                           <option>D</option>                                          
-                                        </select>
-                                </td>
-                                <td>
-                                    <label class="control-label inline">.doc</label> 
-                                    <div class="input-append">
-                                        <span class="add-on"><strong>D</strong></span>
-                                   </div>
-                                  
-                                </td>
-                              
-                            </tr>
+                              </tr>
                         </tbody>
                     </table>
                 
-                
+<ul class="pager wizard">
+                <li class="previous" ><a href="#">Previous</a></li>
+                <li class="next"><a href="#">Next</a></li>
+                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+            </ul>
                 </div>      
     </div>
             <div class="tab-pane" id="tab5">
@@ -249,7 +178,12 @@ numbers is to click in the cell below the numbers and then:</b></label></br>
                               </tr>
                             
                         </tbody>
-                    </table>                               
+                    </table> 
+                    <ul class="pager wizard">
+                <li class="previous" ><a href="#">Previous</a></li>
+                <li class="next"><a href="#">Next</a></li>
+                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+            </ul>
                 </div>      
             </div>     
     </div>
