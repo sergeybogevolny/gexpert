b741527f483d159b6bf689570da72ed8372d13f5
smartguy07 <smartguy07@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-17 18:50:37 +0000

    Instruction page and results page

---
+++ src/templates/instructions.php
@@ -0,0 +1,45 @@
+<form method="POST" class="form-horizontal">
+
+    <legend>Rules /Instructions</legend> 
+     <div class="container-fluid">
+         <h2>Taking the Quiz</h2>
+				  <ol>
+					<li>
+					  <p>Select an answer for every question. Unanswered questions will be scored as incorrect.</p>
+					</li>
+					<li>
+					  <p>There are three possible question types:
+					  </p>
+					  <ul>
+						<li>
+							<p><strong>Multiple Choice</strong>: click the radio button to indicate your choice. Currently, only one answer can be selected for a multiple choice question.</p>
+						</li>
+						<li>
+							<p><strong>True/False</strong>: click the radio button to indicate your choice.</p>
+						</li>
+						<li>
+							<p><strong>Matching Answers</strong>: select a match from the pop-up list below each item. </p>
+						</li>
+						<li>
+						  <p>If you use a wheel button mouse, take care not to accidently change your answers. Sometimes scrolling the wheel will rotate through the answers in a selection list, when you might have meant simply to scroll farther down in the quiz window. </p>
+						</li>
+					  </ul>
+					</li>
+					<li>
+					  <p>Click on the <strong>Submit</strong> button at the bottom of the page to have your answers graded.</p>
+					</li>
+					<li>
+					  <p>You will be shown your results, including your score and any feedback offered by the author of the quiz. You might wish to print this page for your own records. At this stage, you might be able to check your answers: see below.</p>
+					</li>
+					<li>
+					  <p>If you want to try to get a better score, click the <strong>Try Again</strong> button at the bottom of the results page. You can try the quiz as many times as you like.</p>
+					</li>
+					<li>
+					  <p>If you are completing the quiz as part of a class assignment, use the <strong>Print</strong> command from your web browser to print the results page for your instructor, and for your own records.</p>
+					</li>
+				  </ol>
+         <button type="button" class="btn-primary">Start Test</button>
+
+     </div>
+    
+</form>
---
+++ src/templates/results.php
@@ -0,0 +1,8 @@
+<form method="POST" class="form-horizontal">
+
+    <legend>Results</legend> 
+     <div class="container-fluid">
+         <h2>Your Score</h2>
+    </div>
+    
+</form>
