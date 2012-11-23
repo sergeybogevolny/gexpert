201c213ac2430a5294eae1e776529e11c5393e35
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2012-11-23 12:03:14 +0000

    load questions page

--- trunk/src/templates/question.php
+++ trunk/src/templates/question.php
@@ -21,134 +21,134 @@
                 <legend>Question (1 of 10)</legend>  
                 <div class="control-group">
                     <label class="control-label" for="question1"><b> A fast way to add up this column of
-numbers is to click in the cell below the numbers and then:</b></label></br>
+                            numbers is to click in the cell below the numbers and then:</b></label></br>
                     <div class="controls">
                         <label class="checkbox">                          
-                                        <input type="checkbox" name="multipleanswer_1" id="multipleanswer_1">
-                          <strong>a)</strong> Click Subtotals on the Data menu.
+                            <input type="checkbox" name="multipleanswer_1" id="multipleanswer_1">
+                            <strong>a)</strong> Click Subtotals on the Data menu.
                         </label>
                     </div>
                     <div class="controls">
                         <label class="checkbox">                          
-                                        <input type="checkbox" name="multipleanswer_2" id="multipleanswer_2">
-                          <strong>b)</strong> View the sum in the formula bar.
+                            <input type="checkbox" name="multipleanswer_2" id="multipleanswer_2">
+                            <strong>b)</strong> View the sum in the formula bar.
                         </label>
                     </div>
                     <div class="controls">
                         <label class="checkbox">                          
-                                        <input type="checkbox" name="multipleanswer_3" id="multipleanswer_3">
-                          <strong>c)</strong> Click the AutoSum button on the Standard toolbar, then press ENTER.
+                            <input type="checkbox" name="multipleanswer_3" id="multipleanswer_3">
+                            <strong>c)</strong> Click the AutoSum button on the Standard toolbar, then press ENTER.
                         </label>
                     </div>
                     <div class="controls">
                         <label class="checkbox">                          
-                                        <input type="checkbox" name="multipleanswer_4" id="multipleanswer_4">
-                          <strong>d)</strong> Click the AutoSum button on the Advanced toolbar, then press ESC.
+                            <input type="checkbox" name="multipleanswer_4" id="multipleanswer_4">
+                            <strong>d)</strong> Click the AutoSum button on the Advanced toolbar, then press ESC.
                         </label>
                     </div>
-                  
-<ul class="pager wizard">
-                <li class="previous" ><a href="#">Previous</a></li>
-                <li class="next"><a href="#">Next</a></li>
-                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
-            </ul>
-        </div>	
-    </div>
+
+                    <ul class="pager wizard">
+                        <li class="previous" ><a href="#">Previous</a></li>
+                        <li class="next"><a href="#">Next</a></li>
+                        <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+                    </ul>
+                </div>	
+            </div>
             <div class="tab-pane" id="tab2">
 
                 <legend>Question (2 of 10)</legend>  
                 <div class="control-group">
                     <label class="control-label" for="question1"><b> A fast way to add up this column of
-numbers is to click in the cell below the numbers and then:</b></label></br>
+                            numbers is to click in the cell below the numbers and then:</b></label></br>
                     <div class="controls">
                         <label class="radio">                          
-                                        <input type="radio" name="correctanswer_1" id="correctanswer_1">
-                          <strong>a)</strong> Click Subtotals on the Data menu.
+                            <input type="radio" name="correctanswer_1" id="correctanswer_1">
+                            <strong>a)</strong> Click Subtotals on the Data menu.
                         </label>
                     </div>
 
                     <div class="controls">
                         <label class="radio">                          
-                                        <input type="radio" name="correctanswer_2" id="correctanswer_2">
-                          <strong>b)</strong> View the sum in the formula bar.
+                            <input type="radio" name="correctanswer_2" id="correctanswer_2">
+                            <strong>b)</strong> View the sum in the formula bar.
                         </label>
                     </div>
                     <div class="controls">
                         <label class="radio">                          
-                                        <input type="radio" name="correctanswer_3" id="correctanswer_3">
-                          <strong>c)</strong> Click the AutoSum button on the Standard toolbar, then press ENTER.
+                            <input type="radio" name="correctanswer_3" id="correctanswer_3">
+                            <strong>c)</strong> Click the AutoSum button on the Standard toolbar, then press ENTER.
                         </label>
                     </div>
                     <div class="controls">
                         <label class="radio">                          
-                                        <input type="radio" name="correctanswer_4" id="correctanswer_4">
-                          <strong>d)</strong> Click the AutoSum button on the Advanced toolbar, then press ESC.
+                            <input type="radio" name="correctanswer_4" id="correctanswer_4">
+                            <strong>d)</strong> Click the AutoSum button on the Advanced toolbar, then press ESC.
                         </label>
                     </div>
-                  
-<ul class="pager wizard">
-                <li class="previous" ><a href="#">Previous</a></li>
-                <li class="next"><a href="#">Next</a></li>
-                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
-            </ul>
-        </div>	
-    </div>
+
+                    <ul class="pager wizard">
+                        <li class="previous" ><a href="#">Previous</a></li>
+                        <li class="next"><a href="#">Next</a></li>
+                        <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+                    </ul>
+                </div>	
+            </div>
             <div class="tab-pane" id="tab3">
 
                 <legend>Question (3 of 10)</legend>  
                 <div class="control-group">
                     <label class="control-label" for="question1"><b> There are three worksheets with every new workbook. You can change that automatic number if you want to.  
-                    </b></label><br>
-          <div class='toggle alternative' data-enabled="ON" data-disabled="OFF" data-toggle="toggle">
-            <input type="checkbox" value="1" name="myCheckbox" class="checkbox" checked="checked" />
-            <label class="check" for="myCheckbox"></label>
-          </div>                        
-<ul class="pager wizard">
-                <li class="previous" ><a href="#">Previous</a></li>
-                <li class="next"><a href="#">Next</a></li>
-                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
-            </ul>
-        </div>	
-    </div>
+                        </b></label><br>
+                    <div class='toggle alternative' data-enabled="ON" data-disabled="OFF" data-toggle="toggle">
+                        <input type="checkbox" value="1" name="myCheckbox" class="checkbox" checked="checked" />
+                        <label class="check" for="myCheckbox"></label>
+                    </div>                        
+                    <ul class="pager wizard">
+                        <li class="previous" ><a href="#">Previous</a></li>
+                        <li class="next"><a href="#">Next</a></li>
+                        <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+                    </ul>
+                </div>	
+            </div>
             <div class="tab-pane" id="tab4">
 
                 <legend>Question (4 of 10)</legend>  
                 <div class="control-group">
                     <label class="control-label" for="question1"><b>Arrange in Right Sequence</b></label></br>
-                <table class="table table-striped table-bordered table-hover .table-condensed">
+                    <table class="table table-striped table-bordered table-hover .table-condensed">
                         <thead>
                             <tr>
                                 <th>Arrange in Right Sequence</th>                              
-                                                          </tr>
+                            </tr>
                         </thead>
                         <tbody>
-               <tr>
-                                
+                            <tr>
+
                                 <td>
                                     <ul id="sortable">
-                                       <li class="ui-state-default">mdb</li>
-                                       <li class="ui-state-default">doc</li>
-                                       <li class="ui-state-default">xls</li>
-                                       <li class="ui-state-default">ppt</li>                                       
-                                   </ul>
-                               
-                              </tr>
+                                        <li class="ui-state-default">mdb</li>
+                                        <li class="ui-state-default">doc</li>
+                                        <li class="ui-state-default">xls</li>
+                                        <li class="ui-state-default">ppt</li>                                       
+                                    </ul>
+
+                            </tr>
                         </tbody>
                     </table>
-                
-<ul class="pager wizard">
-                <li class="previous" ><a href="#">Previous</a></li>
-                <li class="next"><a href="#">Next</a></li>
-                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
-            </ul>
+
+                    <ul class="pager wizard">
+                        <li class="previous" ><a href="#">Previous</a></li>
+                        <li class="next"><a href="#">Next</a></li>
+                        <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+                    </ul>
                 </div>      
-    </div>
+            </div>
             <div class="tab-pane" id="tab5">
 
                 <legend>Question (5 of 10)</legend>  
                 <div class="control-group">
                     <label class="control-label" for="question1"><b> Match the following file extensions</b></label></br>
-                <table class="table table-striped table-bordered table-hover .table-condensed">
+                    <table class="table table-striped table-bordered table-hover .table-condensed">
                         <thead>
                             <tr>
                                 <th>Question</th>                              
@@ -157,54 +157,54 @@ numbers is to click in the cell below the numbers and then:</b></label></br>
                         </thead>
                         <tbody>
                             <tr>
-                                
+
                                 <td>
-                                   <ul>
-                                       <li>MS Word</li>
-                                       <li>MS Excel</li>
-                                       <li>MS Powerpoint</li>
-                                       <li>MS Access</li>                                       
-                                   </ul>
-                                 
+                                    <ul>
+                                        <li>MS Word</li>
+                                        <li>MS Excel</li>
+                                        <li>MS Powerpoint</li>
+                                        <li>MS Access</li>                                       
+                                    </ul>
+
                                 </td>
                                 <td>
                                     <ul id="sortable1">
-                                       <li class="ui-state-default">mdb</li>
-                                       <li class="ui-state-default">doc</li>
-                                       <li class="ui-state-default">xls</li>
-                                       <li class="ui-state-default">ppt</li>                                       
-                                   </ul>
+                                        <li class="ui-state-default">mdb</li>
+                                        <li class="ui-state-default">doc</li>
+                                        <li class="ui-state-default">xls</li>
+                                        <li class="ui-state-default">ppt</li>                                       
+                                    </ul>
                                 </td>
-                              </tr>
-                            
+                            </tr>
+
                         </tbody>
                     </table> 
                     <ul class="pager wizard">
-                <li class="previous" ><a href="#">Previous</a></li>
-                <li class="next"><a href="#">Next</a></li>
-                <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
-            </ul>
+                        <li class="previous" ><a href="#">Previous</a></li>
+                        <li class="next"><a href="#">Next</a></li>
+                        <li class="next finish" style="display:none;"><a href="javascript:;">Finish</a></li>
+                    </ul>
                 </div>      
             </div>     
+        </div>
     </div>
-    </div>
-            
+
 </form>
 
 <script>
     $('.alternative').toggle({
         onClick: function (evt, status) {
-          console.log('.alternative changed to: ' + status, 'the reverse of: ' + !status);
+            console.log('.alternative changed to: ' + status, 'the reverse of: ' + !status);
         },
         text: {
-          enabled: 'TRUE',
-          disabled: 'FALSE'
+            enabled: 'TRUE',
+            disabled: 'FALSE'
         },
         style: {
-          enabled: 'success',
-          disabled: 'danger'
+            enabled: 'success',
+            disabled: 'danger'
         }
-      });
+    });
     $(document).ready(function() {
         $('#rootwizard').bootstrapWizard();
         $( "#sortable,#sortable1" ).sortable();
