0b784fc39b4b28d39d6db159faa9ede42ffd8814
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2013-02-12 16:07:20 +0000

    update for create test

--- trunk/tech/test.php
+++ trunk/tech/test.php
@@ -1,5 +1,4 @@
 <?php
-
 //$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
 //    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 //    $key = "This is a very secret key";
@@ -8,24 +7,29 @@
 //
 //    $crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_ECB, $iv);
 //    echo $crypttext . "\n";
-//    
-
-$pool = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
-$countPool = strlen($pool);
-$totalChars = 16;
-
-$serial = '';
-for ($i = 0; $i < $totalChars; $i++) {
-    //echo $countPool;
-    //echo '<br/>';
-    $currIndex = mt_rand(0, (int) $countPool);
-    //echo $currIndex;
-    // echo '<br/>';
-    $currChar = $pool[$currIndex];
-//    echo '<br/>';
-    $serial .= $currChar;
-//    echo '<br/>';
-}
-echo explode($serial);
-
+//
+//$pool = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
+//$countPool = strlen($pool);
+//$totalChars = 16;
+//
+//$serial = '';
+//for ($i = 0; $i < $totalChars; $i++) {
+//    //echo $countPool;
+//    //echo '<br/>';
+//    $currIndex = mt_rand(0, (int) $countPool);
+//    //echo $currIndex;
+//    // echo '<br/>';
+//    $currChar = $pool[$currIndex];
+////    echo '<br/>';
+//    $serial .= $currChar;
+////    echo '<br/>';
+//}
+//echo explode($serial);
 ?>
+<html>
+    <body>
+        <ul> <li> <ol> <li>A</li> <li>B</li> <ul> <li>Item 1</li> <li>Item 2</li> </ul> <li>C</li> </ol> </li> </ul>
+
+    </body>
+    <#60&;>
+</html>
