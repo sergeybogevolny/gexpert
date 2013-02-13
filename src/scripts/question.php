bc91931afaa17eb547ba3588b3d9becdc9f519ee
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2013-02-13 18:49:12 +0000

    Bug fix

--- src/scripts/question.php
+++ src/scripts/question.php
@@ -33,7 +33,7 @@ if ($_POST["test_id"]) {
                     $selected_array = json_decode($current_answer);
                     $answercnt = count($correctanswers);
                     foreach ($correctanswers as $key1 => $value1) {
-                    if(is_array($selected_array){
+                    if(is_array($selected_array)){
                         if (in_array($value1['id'], $selected_array)) {
                             $scores+=(1 / $answercnt);
                             $correctanswercnt[$value["id"]]++;
