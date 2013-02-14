abe00d6df6b62c941726c1df996b7ed8ae240425
meenakshi.sun20@gmail.com <meenakshi.sun20@gmail.com@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2013-02-14 15:27:27 +0000

    header already sent problem

--- trunk/index.php
+++ trunk/index.php
@@ -29,7 +29,6 @@ if ($_POST['type'] != 'ajax' && $_GET['type'] != 'ajax') {
     <!DOCTYPE html>
     <html lang="en">
         <head>
-            <meta charset="utf-8">
             <title>g-Xpertize</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="description" content="">
