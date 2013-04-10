7dad46bac47ed9196943729f92e06e078d0e67c6
smartguy07 <smartguy07@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2013-04-10 15:45:48 +0000

    Theme fixed

--- src/scripts/tests.php
+++ src/scripts/tests.php
@@ -6,4 +6,6 @@ $cTestControllerObj = new cTestController();
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ $pagename = "Test Available";
+ $pagedescription = "Take a Test";
 ?>
--- src/templates/login.php
+++ src/templates/login.php
@@ -5,17 +5,22 @@ $cFormObj->options["alert"]["data"] = $_GET['m'];
 $cFormObj->addAlert();
 echo $cFormObj->html();
 ?>
-<form method="POST" class="form-horizontal">
 
-    <div class="pull-right">
-        <legend>g-Xpertize Login</legend>
+<form method="POST" class="form-horizontal">
+<div class="container-fluid">
+<div class="span6" align="center">
+      <img src="<?php echo ClientLogo;?>"  /></br>
+	  <h1><?php echo ClientName;?></h1>
+      </div>
+    <div class="span5">
+      <legend>g-Xpertize Login</legend>
+      
         <div class="control-group">
             <label class="control-label" for="username">Username</label>
             <div class="controls">
                 <div class="input-prepend">
                     <span class="add-on">
-                        <i class="icon-user"></i>
-                    </span>
+                        <i class="icon-user"></i>                    </span>
                     <input name="loginname" type="text" id="username" value="" class="username" placeholder="Username" required/>
                 </div>
             </div>
@@ -25,8 +30,7 @@ echo $cFormObj->html();
             <div class="controls">
                 <div class="input-prepend">
                     <span class="add-on">
-                        <i class="icon-lock"></i>
-                    </span>
+                        <i class="icon-lock"></i>                    </span>
                     <input name="password" type="password" id="password" class="password" placeholder="Password" required/>
                 </div>
             </div>
@@ -39,11 +43,9 @@ echo $cFormObj->html();
                 <input type="submit" name="Submit" value="Login" class="btn btn-primary" />
                 <a href="<?php echo $cFormObj->createLinkUrl(array('f' => 'forget_password')); ?>">Forget Password</a>
                 <div align="left">New User ?&nbsp;
-                    <a href="<?php echo $cFormObj->createLinkUrl(array('f' => 'register')); ?>">Click here to register</a>
-                </div>
+                    <a href="<?php echo $cFormObj->createLinkUrl(array('f' => 'register')); ?>">Click here to register</a>                </div>
             </div>
         </div>
-
     </div>
-
+</div>
 </form>
