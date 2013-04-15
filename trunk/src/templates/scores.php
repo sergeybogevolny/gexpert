6fca19cf058a5b423744549c98f716328fa15253
meenakshi.sun20 <meenakshi.sun20@3cd6457f-63e5-c5ca-0abf-5988a218152f>
2013-04-15 14:30:39 +0000

    title for chart in scores

--- trunk/src/templates/scores.php
+++ trunk/src/templates/scores.php
@@ -43,14 +43,16 @@
     $table = $cFormObj->html();
     $cFormObj->data = $scores;
     $cFormObj->options['id'] = "test";
-    $cFormObj->options['title'] = "Sundar";
+
     $cFormObj->options['chart_type'] = 'line';
     $cFormObj->options['axis']['x']['name'] = array('name' => "Test", 'type' => "string", 'sort' => true, 'chart' => 'line', 'filter' => 'box');
     //$cFormObj->options['axis_x']['username'] = array('name' => "User", 'type' => "string", 'sort' => true, 'chart' => 'line', 'filter' => 'box');
     //$cFormObj->options['axis']['x']['username'] = array('name' => "User", 'type' => "string", 'sort' => true, 'chart' => 'line', 'filter' => 'box');
     if ($_SESSION['user_type'] > 1) {
+        $cFormObj->options['title'] = "My Scores";
         $cFormObj->options['axis']['y']['score'] = array('name' => "Score", 'type' => "number", 'sort' => true, 'chart' => 'line', "agg_function" => "sum", 'filter' => 'box');
     } else {
+        $cFormObj->options['title'] = "Number of user attended VS Tests";
         $cFormObj->options['axis']['y']['score'] = array('name' => "Score", 'type' => "number", 'sort' => true, 'chart' => 'line', "agg_function" => "count", 'filter' => 'box');
     }
 
